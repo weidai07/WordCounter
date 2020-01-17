53102ed023f8fbf2c597267954b2efaf7e6459f6
Student <example@example.com>
2020-01-17 15:08:39 -0800

    fix previous build error

--- WordCounter.Solutions/Program.cs
+++ WordCounter.Solutions/Program.cs
@@ -1,19 +1,33 @@
 using System;
 using System.Collections.Generic;
 
-namespace WordCounter.Models 
-{
-  public class Program 
-  {
+namespace WordCounterCheck.Models {
+  public class Program {
     static void Main()
     {
-      Console.WriteLine("Welcome to Word Counter Program!"); 
-      Console.WriteLine("Please Enter a Sentence!");
-      string UserSentence = Console.ReadLine();
-      Console.WriteLine("Now Enter a Word!");
-      string UserWord = Console.ReadLine();
-      Console.WriteLine("No, the word you entered does not match any words from your sentence.");
-      Console.WriteLine("Yes, the word you entered does match a word or words from your sentence.");
-    } 
+    
+      Console.WriteLine("Please Enter a Sentence");
+      string UserInput = Console.ReadLine().ToLower();
+      WordCounter MyWord = new WordCounter(UserInput);
+      while(UserInput != "1")
+      { 
+        Console.WriteLine("Please Enter the Words You Would Like to Search");
+        UserInput = Console.ReadLine().ToLower();
+        if (UserInput == "1")
+        {
+          break;
+        }
+        else
+        {
+          MyWord.AddWord(UserInput);
+        }
+      }
+      List<string> results = MyWord.Check();
+      Console.WriteLine("Matches: ");
+      foreach(string w in results)
+      {
+        Console.WriteLine(w);
+      }
+    }
   }
 }
--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -3,11 +3,20 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WordCounter.Models;
 using System.Collections.Generic;
 
-namespace WordCounter.Tests
+namespace WordCounterCheck.Tests
 {
     [TestClass]
     public class WordCounterTester
     {
+        [TestMethod]
+        public void Word_CheckWord_Conversation()
+        {
+            Word newWord = new Word("dave");
+            newWord.AddWord("vade");
+            newWord.AddWord("deva");
+            List<string> results = newWord.Check();
+            Assert.AreEqual("deva",results[1]);
+        } 
  
     }
 }
